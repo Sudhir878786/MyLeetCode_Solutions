4fb41de516ef2c4d8705792b952c49b5d89d3ae2
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2022-07-23 20:32:32 +0530

    Time: 1087 ms (33.01%), Space: 209.2 MB (45.40%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,26 +1,121 @@
 
    
+             // Solution 1
 
-
-class Solution {
-public:
+// class Solution {
+// public:
   
     
-    vector<int> countSmaller(vector<int>& nums) {
-        vector<int> ans(nums.size(), 0);
-        // allocate enough space for BIT of range [1, 20001] 
-        vector<int> bit(20002, 0);
-        for (int i = nums.size() - 1; i >= 0 ; i--) {
-            // get count that less or equals num-1 by BIT, (plus 10001 to move number range to [1, 20001])
-            for (int j = nums[i] + 10000; j > 0; j -= j & -j) {  
-                ans[i] += bit[j];
+//     vector<int> countSmaller(vector<int>& nums) {
+//         vector<int> ans(nums.size(), 0);
+//         // allocate enough space for BIT of range [1, 20001] 
+//         vector<int> bit(20002, 0);
+//         for (int i = nums.size() - 1; i >= 0 ; i--) {
+//             // get count that less or equals num-1 by BIT, (plus 10001 to move number range to [1, 20001])
+//             for (int j = nums[i] + 10000; j > 0; j -= j & -j) {  
+//                 ans[i] += bit[j];
+//             }
+//             // update count in BIT, (plus 10001 to move number range to [1, 20001])
+//             for (int j = nums[i] + 10001; j < bit.size(); j+= j & -j) {
+//                 bit[j]++;
+//             }
+//         }
+//         return ans;
+        
+//     }
+// };
+
+
+     // Solution 2
+class Solution {
+public:
+    
+    void merge(vector<int> &count, vector<pair<int, int>> &v, int left, int mid, int right){
+        vector<pair<int,int>> temp(right - left + 1);
+        int i=left;
+        int j=mid+1;
+        int k=0;
+        
+        while(i<=mid && j<=right){
+            
+            if(v[i].first <= v[j].first){
+                temp[k++] = v[j++];
             }
-            // update count in BIT, (plus 10001 to move number range to [1, 20001])
-            for (int j = nums[i] + 10001; j < bit.size(); j+= j & -j) {
-                bit[j]++;
+            
+            else{
+                
+                count[v[i].second] += right-j+1;
+                temp[k++] = v[i++];
+                
             }
+            
         }
-        return ans;
+        
+        
+        while(i<=mid){
+            temp[k++] = v[i++];
+        }
+        
+        while(j<=right){
+            temp[k++] = v[j++];
+        }
+        
+        for(int i=left;i<=right;i++){
+            v[i] = temp[i-left]; 
+        }
+        
+    }
+    
+    void mergeSort(vector<int> &count, vector<pair<int,int>> &v, int left, int right){
+        
+        
+        if(left<right){
+            int mid = left + (right-left)/2;
+            mergeSort(count, v, left, mid);
+            mergeSort(count, v, mid+1, right);
+            merge(count, v, left, mid, right);
+        }
+        
+    }
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int,int>> v(n);
+        for(int i=0;i<n;i++){
+            pair<int, int> p;
+            p.first = nums[i];
+            p.second = i;
+            v[i] = p;
+        }
+        
+        
+        vector<int> count(n, 0);
+        mergeSort(count, v, 0, n-1);
+        return count;
         
     }
 };
+
+
+   
+// TLE 
+
+// // class Solution {
+// // public:
+  
+    
+// //     vector<int> countSmaller(vector<int>& nums) {
+// //         vector<int> ans(nums.size(), 0);
+// //         for(int i=0;i<nums.size();i++){
+// //             for(int j=i+1;j<nums.size();j++){
+// //                 if(nums[i]>nums[j]){
+// //                     ans[i]++;
+// //                 }
+// //             }
+// //         }
+        
+       
+// //         return ans;
+        
+// //     }
+// // };
