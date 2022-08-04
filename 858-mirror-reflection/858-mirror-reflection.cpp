dc39014488be4691630fc799809af351ed28d470
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2022-08-04 12:31:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+while (p%2==0 && q%2==0) {
+		p = p / 2;
+		q = q / 2;
+	}
+	
+	// conclusion
+	if (p%2 == 0)
+		return 2;
+	
+	if (q%2 == 0)
+		return 0;
+	
+	return 1;
+        }
+};
