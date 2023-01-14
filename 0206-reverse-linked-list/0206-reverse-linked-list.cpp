f29db200e009ab75bb7f815373978af1ff8c3b22
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-01-14 15:05:24 +0530

    Time: 7 ms (79.94%), Space: 8.5 MB (19.22%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(!head || !head->next)return head;
-        auto temp=reverseList(head->next);
+        ListNode* temp=reverseList(head->next);
         head->next->next=head;
         head->next=NULL;
         return temp;
