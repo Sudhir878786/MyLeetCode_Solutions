0525acc19f3ed5b3739f54cc1776b334a4fa29ab
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-02-21 13:05:01 +0530

    Time: 25 ms (82.58%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto it:nums){
-            mp[it]++;
-        }
+        // unordered_map<int,int>mp;
+        // for(auto it:nums){
+        //     mp[it]++;
+        // }
+        // int ans=0;
+        // for(auto it=mp.begin();it!=mp.end();it++){
+        //     if(it->second==1){
+        //         ans=it->first;
+        //     }
+        // }
+        // return ans;
         int ans=0;
-        for(auto it=mp.begin();it!=mp.end();it++){
-            if(it->second==1){
-                ans=it->first;
-            }
+        for(auto it:nums){
+            ans^=it;
         }
         return ans;
     }
