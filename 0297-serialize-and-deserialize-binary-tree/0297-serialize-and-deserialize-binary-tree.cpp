8b4d381518beeaaa0b6e2f39fbaeb91cac8289c0
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-02-23 17:46:33 +0530

    Time: 70 ms (28.82%), Space: 40.2 MB (22.47%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,31 @@
+class Codec {
+public:
+
+    string serialize(TreeNode* root) 
+    {
+        return !root ? " null" : " " + to_string(root->val) + serialize(root->left) + serialize(root->right);
+    }
+	
+    TreeNode* deserialize(string data) 
+    {
+        istringstream ss(data);
+        return buildTree(ss);
+    }
+    
+
+    
+    TreeNode* buildTree(istringstream& ss)
+    {
+        string s;
+        ss >> s;
+        
+        if (s == "null")
+			return NULL;
+        
+        TreeNode* node = new TreeNode(stoi(s));
+        node->left = buildTree(ss);
+        node->right = buildTree(ss);
+            
+        return node;
+    }
+};
