249f33bac996bf0c12a90fbe44b411c89972d98f
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-02-24 16:52:15 +0530

    Time: 24 ms (66.68%), Space: 14.1 MB (47.44%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool dfs( vector<int>adj[], vector<int>&vis,int id)
+    {
+         if(vis[id]==1)
+            return true;
+        if(vis[id]==0){
+            vis[id]=1;
+            for(auto edge : adj[id]){
+                if(dfs(adj,vis,edge))
+                    return true;
+            }
+        }
+        vis[id] = 2;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& p) {
+        
+        vector<int>adj[n];
+        for(auto it:p){
+            int u=it[0];
+            int v=it[1];
+            adj[v].push_back(u);
+            //adj[v].push_back(u);
+            
+        }
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
+            if(dfs(adj,vis,i)){
+                return 0;
+            }
+        }
+        return 1;
+        
+    }
+};
