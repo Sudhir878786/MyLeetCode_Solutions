b62b5da4bf8c8a62d216332f89a05c5713117ed2
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-02-25 16:12:27 +0530

    Time: 240 ms (42.32%), Space: 74.1 MB (11.30%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode*merge(ListNode*l1,ListNode*l2)
+    {
+         ListNode *ptr = new ListNode(0);
+        ListNode *curr = ptr;
+        
+        while(l1 != NULL && l2 != NULL)
+        {
+            if(l1->val <= l2->val)
+            {
+                curr -> next = l1;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                curr -> next = l2;
+                l2 = l2 -> next;
+            }
+        
+        curr = curr ->next;
+        
+        }
+        
+        //for unqual length linked list
+        
+        if(l1 != NULL)
+        {
+            curr -> next = l1;
+            l1 = l1->next;
+        }
+        
+        if(l2 != NULL)
+        {
+            curr -> next = l2;
+            l2 = l2 ->next;
+        }
+        
+        return ptr->next;
+        
+       
+        
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(!head)return NULL;
+        if(!head->next){
+            return head;
+        }
+        ListNode*temp=NULL;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL and fast->next!=NULL){
+            temp=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        temp->next=NULL;
+        ListNode*a=sortList(head);
+        ListNode*b=sortList(slow);
+        return merge(a,b);
+    }
+};
