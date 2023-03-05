5dde2b26f8b803feb5949f2379cfeef6d09a2036
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-03-06 03:30:26 +0530

    Time: 321 ms (59.24%), Space: 10.6 MB (38.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,31 @@
-class Solution {
-public:
-    int func(int i,int n,int prev,vector<int>&nums,vector<vector<int>>&dp)
-    {
-        if(i==n)return 0;
-        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
-        int npic=0+func(i+1,n,prev,nums,dp);
-        int nott=0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            nott=1+func(i+1,n,i,nums,dp);
+class Solution
+{
+    public:
+        int func(int i, int n, int prev, vector<int> &nums, vector<vector< int>> &dp)
+        {
+            if (i == n) return 0;
+            if (dp[i][prev + 1] != -1) return dp[i][prev + 1];
+            int npic = 0 + func(i + 1, n, prev, nums, dp);
+            int nott = 0;
+            if (prev == -1 || nums[i] > nums[prev])
+            {
+                nott = 1 + func(i + 1, n, i, nums, dp);
+            }
+            return dp[i][prev + 1] = max(nott, npic);
         }
-        return  dp[i][prev+1]=max(nott,npic);
-    }
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS(vector<int> &nums)
+    {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return func(0,n,-1,nums,dp);
+        vector<int>dp(n+1,1);
+        int ans=1;
+        for(int i=0;i<n;i++){
+            for(int pre=0;pre<i;pre++){
+                if(nums[i]>nums[pre]){
+                    dp[i]=max( dp[i] ,1+dp[pre]);
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
     }
 };
