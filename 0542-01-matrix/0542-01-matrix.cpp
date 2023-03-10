e57ab1c42c6e4262fc3da246a792cafb2e6d58b3
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-03-10 15:39:22 +0530

    Time: 104 ms (29.32%), Space: 31.7 MB (54.85%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    dist[i][j]=0;
+                    q.push({{i,j},0});
+                    // vis[i][j]=1;
+                }
+                // else vis[i][j]=0;
+            }
+        }
+        int delrow[] = {-1, 0, +1, 0}; 
+	    int delcol[] = {0, +1, 0, -1}; 
+	    
+	    // traverse till queue becomes empty
+	    while(!q.empty()) {
+	        int row = q.front().first.first; 
+	        int col = q.front().first.second; 
+	        int steps = q.front().second; 
+	        q.pop(); 
+	        // dist[row][col] = steps; 
+	        // for all 4 neighbours
+	        for(int i = 0;i<4;i++) {
+	            int nrow = row + delrow[i]; 
+	            int ncol = col + delcol[i]; 
+	            // check for valid unvisited cell
+	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
+	           ) {
+	                // vis[nrow][ncol] = 1; 
+                    if(dist[nrow][ncol]>dist[row][col]+1){
+                        dist[nrow][ncol]=dist[row][col]+1;
+                        q.push({{nrow, ncol}, steps+1});  
+                    }
+	                
+	            }
+	        }
+	    }
+	    // return distance matrix
+	    return dist; 
+    }
+};
