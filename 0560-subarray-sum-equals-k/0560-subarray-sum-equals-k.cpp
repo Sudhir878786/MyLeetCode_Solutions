96077fa3fbcca4ec3d9c0997d4d496303ea1c7b1
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-03-15 21:38:11 +0530

    Time: 105 ms (45.12%), Space: 41.4 MB (86.10%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int ans=0,sum=0;
         unordered_map<int,int>mp;
+        int ans=0,sum=0;
         mp[0]=1;
         for(auto it:nums){
-           sum+=it;
-           if(mp.find(sum-k)!=mp.end()){
-               ans+=mp[sum-k];
-           }
-           mp[sum]++;
-       }
+            sum+=it;
+            if(mp.find(sum-k)!=mp.end()){
+                ans+=mp[sum-k];
+            }
+            
+                mp[sum]++;
+        }
+        // for(auto it=mp.begin();it!=mp.end();it++){
+        //     cout<<it->first<<"->"<<it->second<<endl;
+        // }
         return ans;
     }
 };
