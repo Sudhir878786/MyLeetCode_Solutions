c1345aef5cd1665b7db16586e0f7c69b582c273b
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-04-11 16:15:48 +0530

    Time: 8 ms (51.36%), Space: 10.9 MB (11.36%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+        int minOperations(vector<string> &logs)
+        {
+            stack<string> st;
+            for (int i = 0; i < logs.size(); i++)
+            {
+                if (logs[i] == "../")
+                {
+                    if (!st.empty())
+                    {
+                        st.pop();
+                    }
+                }
+                else if (logs[i] == "./")
+                {
+                    continue;
+                }
+                else st.push(logs[i]);
+            }
+            return st.size();
+        }
+};
