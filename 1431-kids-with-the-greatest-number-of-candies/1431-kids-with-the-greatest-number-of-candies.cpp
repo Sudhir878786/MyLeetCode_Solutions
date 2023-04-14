cfa1f5c19762f5454775450ac8345772ecae53b5
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-04-14 14:21:13 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (86.50%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& can, int ext) {
+          vector<bool>ans;
+        int maxx=*max_element(begin(can),end(can));
+        for(auto it:can){
+            ans.push_back(ext+it >=maxx);
+        }
+        return ans;
+    }
+};
