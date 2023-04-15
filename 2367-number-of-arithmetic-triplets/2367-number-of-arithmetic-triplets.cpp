14db8be2e2705b90a9759841438085dc1c4de500
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-04-15 12:48:09 +0530

    Time: 7 ms (57.03%), Space: 8.7 MB (66.68%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+        int arithmeticTriplets(vector<int> &nums, int diff)
+        {
+            int n = nums.size();
+            int count = 0;
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                for (int j = i + 1; j < n - 1; j++)
+                {
+                    if (nums[j] - nums[i] == diff)
+                    {
+                        for (int k = j + 1; k < n; k++)
+                        {
+                            if (nums[k] - nums[j] == diff)
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+};
