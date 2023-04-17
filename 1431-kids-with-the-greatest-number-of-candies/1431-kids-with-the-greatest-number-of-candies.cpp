80231520dfbb82682f19e5ae03c4751f03a5d1a1
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-04-17 08:53:49 +0530

    Time: 7 ms (19.72%), Space: 8.8 MB (98.95%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -4,6 +4,7 @@ public:
           vector<bool>ans;
         int maxx=*max_element(begin(can),end(can));
         for(auto it:can){
+            
             ans.push_back(ext+it >=maxx);
         }
         return ans;
