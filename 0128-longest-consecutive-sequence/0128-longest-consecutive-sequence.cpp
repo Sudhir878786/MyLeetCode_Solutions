ca467f03e093215b58410a7ec69680791068fc17
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-05-17 16:27:22 +0530

    Time: 98 ms (78.12%), Space: 43.6 MB (76.34%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,19 @@
-class Solution
-{
-    public:
-        int longestConsecutive(vector<int> &nums)
-        {
-            sort(nums.begin(), nums.end());	// sort array
-            int longest = 1;
-            int current = 1;
-            if(nums.size()==0)return 0;
-            for (int i = 1; i < nums.size(); i++)
-            {
-                if (nums[i] == nums[i - 1]) continue;	// skip if num has already been visited
-                if (nums[i] == nums[i - 1] + 1)
-                {
-                    current++;
-                }
-                else
-                {
-                    longest = max(longest, current);
-                    current = 1;
-                }
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int ans=1;
+        int count=1;
+        if(nums.size()==0)return 0;
+        sort(begin(nums),end(nums));
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])continue;
+            if(nums[i]==nums[i-1]+1)count++;
+            else{
+                ans=max(ans,count);
+                count=1;
             }
-            return max(longest, current);
         }
+        return max(ans,count);
+        
+    }
 };
