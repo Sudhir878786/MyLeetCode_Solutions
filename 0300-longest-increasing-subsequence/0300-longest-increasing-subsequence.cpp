1e3309ab15f6dcc537d8064113bd66844a81be87
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-05-23 10:10:07 +0530

    Time: 310 ms (59.41%), Space: 10.5 MB (70.70%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,7 @@
 class Solution
 {
     public:
-        int func(int i, int n, int prev, vector<int> &nums, vector<vector< int>> &dp)
-        {
-            if (i == n) return 0;
-            if (dp[i][prev + 1] != -1) return dp[i][prev + 1];
-            int npic = 0 + func(i + 1, n, prev, nums, dp);
-            int nott = 0;
-            if (prev == -1 || nums[i] > nums[prev])
-            {
-                nott = 1 + func(i + 1, n, i, nums, dp);
-            }
-            return dp[i][prev + 1] = max(nott, npic);
-        }
+    
     int lengthOfLIS(vector<int> &nums)
     {
         int n=nums.size();
