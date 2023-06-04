8ef74a62e8646ea5d6e80524ce0557ac0b635d3d
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-04 17:59:21 +0530

    Time: 12 ms (85.71%), Space: 46.4 MB (85.71%) - LeetHub

--- 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+  
+    
     int semiOrderedPermutation(vector<int>& nums) {
-        int first, last;
-        for(int i = 0; i < nums.size(); i ++){
-            if(nums[i] == 1){
-                first = i;
-            }else if(nums[i] == nums.size()){
-                last = i;
+        int n=nums.size();
+        int f,l;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                f=i;
+            }
+            else if(nums[i]==nums.size()){
+                l=i;
             }
         }
-        // cout<<"first"<<first<<" and "<<last<<Endl;
-        int answer = first + (nums.size() - 1 - last);
-        if(first > last){
-            answer -= 1;
-            
-        }
-        return answer;
+        int ans= f+(n-1-l);
+        if(f>l)ans-=1;
+        return ans;
     }
 };
