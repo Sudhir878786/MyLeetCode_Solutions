927671d8ce1897d3f0fd91b3f992e5d6f13d6c84
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-05 10:30:38 +0530

    Time: 18 ms (43.93%), Space: 13.4 MB (30.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        int m =matrix[0].size();
-        unordered_set<int>r;
-        unordered_set<int>c;
+        int m=matrix[0].size();
+        unordered_set<int>r,c;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
@@ -15,7 +14,7 @@ public:
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(r.count(i)|| c.count(j)){
+                if(r.count(i)||c.count(j)){
                     matrix[i][j]=0;
                 }
             }
