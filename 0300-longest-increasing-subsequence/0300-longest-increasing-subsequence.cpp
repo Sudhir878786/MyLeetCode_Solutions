c4bb832f7e795abb9337db341356b80503f04b5d
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-05 15:05:35 +0530

    Time: 322 ms (57.21%), Space: 10.4 MB (67.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,19 @@
-class Solution
-{
-    public:
-    
-    int lengthOfLIS(vector<int> &nums)
-    {
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int ans=0;
         int n=nums.size();
         vector<int>dp(n+1,1);
-        int ans=1;
-        for(int i=0;i<n;i++){
-            for(int pre=0;pre<i;pre++){
-                if(nums[i]>nums[pre]){
-                    dp[i]=max( dp[i] ,1+dp[pre]);
+        for(int i{0};i<n;i++){
+            for(int j{0};j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],1+dp[j]);
                 }
+                
             }
-            ans=max(ans,dp[i]);
+        
+                    ans=max(ans,dp[i]);
+                
         }
         return ans;
     }
