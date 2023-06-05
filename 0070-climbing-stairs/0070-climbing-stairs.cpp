a307c9ded4fb599476871e0ce104ea7d7afde27d
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-05 15:54:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[101];
-        dp[1] = 1;
-        dp[2] = 2;
+        int dp[50];
+        dp[1]=1;
+        dp[2]=2;
         dp[3]=3;
-        for(int i = 4 ; i <= n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
         
+        for(int i=4;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
-       
     }
 };
