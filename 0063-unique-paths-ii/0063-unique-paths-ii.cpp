3ec9818aba6bb0cf98ae24293834e35aa71476a3
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-05 17:43:58 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (65.88%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,29 +1,33 @@
-class Solution {
-public:
-    int ans=0;
-    int dfs(int i,int j,vector<vector<int>>&grid,int n,int m,vector<vector<int>>&dp)
-        
+class Solution
+{
+    public:
+        int ans = 0;
+    int dfs(int i, int j, vector<vector < int>> &grid, int n, int m, vector< vector< int>> &dp)
+
     {
-        if(i>=n || j>=m || i<0 || j<0 || grid[i][j]==1){
+        if (i >= n || j >= m || i < 0 || j < 0 || grid[i][j] == 1)
+        {
             return 0;
         }
-        if(i==n-1 and j==m-1)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-       int a= dfs(i+1,j,grid,n,m, dp);
-       int b= dfs(i,j+1,grid,n,m,dp);
-        ans=a+b;
-        return dp[i][j]=ans;
+        if (i == n - 1 and j == m - 1) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
+        int a = dfs(i + 1, j, grid, n, m, dp);
+        int b = dfs(i, j + 1, grid, n, m, dp);
+        ans = a + b;
+        return dp[i][j] = ans;
     }
-    
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int ans=0;
-        int m=grid[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        if(grid[0][0]!=1){
-           return dfs(0,0,grid,n,m,dp);
+
+    int uniquePathsWithObstacles(vector<vector < int>> &grid)
+    {
+        int n = grid.size();
+        int ans = 0;
+        int m = grid[0].size();
+        vector<vector < int>> dp(n + 1, vector<int> (m + 1, -1));
+        if (grid[0][0] != 1)
+        {
+            return dfs(0, 0, grid, n, m, dp);
         }
+
         return 0;
-        
     }
 };
