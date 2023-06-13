22befe52c4c83927541205fbe4fd2410064d5521
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-13 11:03:08 +0530

    Time: 15 ms (76.94%), Space: 10.8 MB (6.70%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,44 +1,56 @@
 class Solution
 {
     public:
-
-       	//BASIC RECURSION 
-        int f(int i, int j, int n, int m, vector<vector < int>> &a, vector< vector< int>> &dp)
+        int func(int j, int i, int n, int m, vector<vector < int>> &matrix, vector< vector< int>> &dp)
         {
-            if (j < 0 || j > m - 1)
+            if (i < 0 || i >= n || j < 0 || j >= m)
+            {
                 return 1e9;
-
-            if (i == 0)
-                return a[i][j];
-
+            }
+            if (i == n - 1)
+            {
+                return matrix[i][j];
+            }
             if (dp[i][j] != -1)
+            {
                 return dp[i][j];
-
-            int up = a[i][j] + f(i - 1, j, n, m, a, dp);
-
-            int ld = a[i][j] + f(i - 1, j - 1, n, m, a, dp);
-
-            int rd = a[i][j] + f(i - 1, j + 1, n, m, a, dp);
-
-            return dp[i][j] = min(up, min(ld, rd));
+            }
+            int a = matrix[i][j] + func(j - 1, i + 1, n, m, matrix, dp);
+            int b = matrix[i][j] + func(j + 1, i + 1, n, m, matrix, dp);
+            int c = matrix[i][j] + func(j, i + 1, n, m, matrix, dp);
+            return dp[i][j] = min(a, min(b, c));
         }
-
     int minFallingPathSum(vector<vector < int>> &matrix)
     {
-
-        int ans = INT_MAX;
-
         int n = matrix.size();
-
         int m = matrix[0].size();
-
-        vector<vector < int>> dp(n, vector<int> (m, -1));
-
-        for (int j = 0; j < m; j++)
-        {
-            ans = min(ans, f(n - 1, j, n, m, matrix, dp));
+        vector<vector < int>> dp(n + 1, vector<int> (m + 1, -1));
+        int ans = INT_MAX;
+        // for (int j = 0; j < m; j++)
+        // {
+        //     ans = min(ans, func(j, 0, n, m, matrix, dp));
+        // }
+        // return ans;
+        for(int j=0;j<m;j++){
+            dp[0][j]=matrix[0][j];
         }
-
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j==0){
+                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j+1]);
+                }
+                else if(j==m-1){
+                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
+                }
+                else {
+                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],min(dp[i-1][j-1],dp[i-1][j+1]));
+                }
+            }
+        }
+        for(int j=0;j<m;j++){
+            ans=min(ans,dp[n-1][j]);
+        }
         return ans;
     }
 };
