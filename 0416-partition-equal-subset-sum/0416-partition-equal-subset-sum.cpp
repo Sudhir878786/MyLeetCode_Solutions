3c9c99993f61b00057d5725af4e8da43efe4ae6c
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-06-14 15:54:06 +0530

    Time: 373 ms (48.67%), Space: 95.8 MB (17.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    bool func(int n,int sum,vector<int>&nums,vector<vector<int>>&dp)
+     bool func(int i,vector<int>&nums,int sum,vector<vector<int>>&dp)
     {
-        if(sum==0)return true;
-        if(n==0)return nums[n]==sum;
-        if(dp[n][sum]!=-1)return dp[n][sum];
-        int nott=func(n-1,sum,nums,dp);
-        int take=0;
-        if(nums[n]<=sum){
-            take=func(n-1,sum-nums[n],nums,dp);
+        if(i==nums.size()){
+            if(sum==0){
+                return true;
+            }else{
+                return false;
+            }
         }
-        return dp[n][sum] =nott|| take;
+         if(dp[i][sum]!=-1)return dp[i][sum];
+        int nottake=func(i+1,nums,sum,dp);
+        int take=0;
+        if(nums[i]<=sum){
+            take=func(i+1,nums,sum-nums[i],dp);
+        }
+        return dp[i][sum]=take||nottake;
     }
     bool canPartition(vector<int>& nums) {
-        int total=accumulate(nums.begin(),nums.end(),0);
-        int n=nums.size();
-        if(total%2==1)return false;
+        int n(nums.size());
+        int sum=accumulate(begin(nums),end(nums),0);
+        if(sum%2==1)return false;
         else {
-            
-            int sum=total/2;
-            vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-            return func(n-1,sum,nums,dp);
+            int cursum=sum/2;
+            vector<vector<int>>dp(n+1,vector<int>(cursum+1,-1));
+            return func(0,nums,cursum,dp);
         }
         return false;
+        
     }
 };
