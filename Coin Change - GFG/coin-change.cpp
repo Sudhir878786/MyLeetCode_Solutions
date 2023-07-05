38e6563b2306144b450ae3a38adfa9881510154a
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-07-05 19:36:24 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,19 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  long long int find(int coins[], int N, int sum,vector<vector<long long int>>&dp)
-  {
-        if(sum==0)return 1;
-        if(sum<0)return 0;
-        if(N<0)return 0;
-        if(dp[N][sum]!=-1)return dp[N][sum];
-        dp[N][sum]= find(coins,N,sum-coins[N],dp)+find(coins,N-1,sum,dp);
-        return dp[N][sum];
-      
-  }
+    long long func(int n,int coins[],int sum ,vector<vector<  long long>>&dp)
+    {
+        if(n==0){
+            return sum%coins[0]==0;
+        }
+        if(dp[n][sum]!=-1){
+            return dp[n][sum];
+        }
+          long long nott=0+func(n-1,coins,sum,dp);
+          long long take=0;
+        if(sum>=coins[n]){
+            take=func(n,coins,sum-coins[n],dp);
+        }
+        return dp[n][sum]=take+nott;
+    }
     long long int count(int coins[], int N, int sum) {
-        vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-1));
-        return find(coins,N-1,sum,dp);
+
+        // code here.
+        vector<vector<long long>>dp(N+1,vector<  long long>(sum+1,-1));
+        return func(N-1,coins,sum,dp);
     }
 };
 
