0e22406657e3542171b429ff3bcf220c2f29f383
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-07-10 10:09:39 +0530

    Time: 67 ms (33.92%), Space: 18.5 MB (67.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-    int prime = 1e9+7;
-    for(int i=0;i<n+1;i++){
-        dp[i][0]=1;
-    }
-    for(int i=1;i<m+1;i++){
-        dp[0][i]=0;
-    }
-    
-    for(int i=1;i<n+1;i++){
-        for(int j=1;j<m+1;j++){
-            
-            if(s[i-1]==t[j-1])
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
-            else
-                dp[i][j] = dp[i-1][j];
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
         }
-    }
-    
-    
-    return dp[n][m];
+        for(int i=1;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=(dp[i-1][j]+dp[i-1][j-1])%1000000007;
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
