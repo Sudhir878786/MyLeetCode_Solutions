b6c266711fef2ef58ac58e26f793df7d2a0e24cb
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-07-11 12:20:24 +0530

    Time: 64 ms (64.76%), Space: 27.9 MB (15.04%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,16 +3,16 @@ public:
     int f(int i,int j, string &s,string &p,vector<vector<int>> &dp)
     {
         if(dp[i][j]!=-1) return dp[i][j];
-        if(i==0 && j==0) return dp[i][j]=1;
-        if(j==0 && i>0) return dp[i][j]=0;
+        if(i==0 && j==0) return dp[i][j]=true;
+        if(j==0 && i>0) return dp[i][j]=false;
         if(i==0 && j>0)
         {
             while(j>0)
             {
                 if(p[j-1]=='*') j--;
-                else return dp[i][j]=0;
+                else return dp[i][j]=false;
             }
-            return dp[i][j]=1;
+            return dp[i][j]=true;
         }
         
         if(s[i-1]==p[j-1] || p[j-1]=='?') return dp[i][j]=f(i-1,j-1,s,p,dp);
@@ -20,12 +20,9 @@ public:
         if(p[j-1]=='*')
         {
             return dp[i][j] = f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp) ;
-            //Two cases
-            //Consider * as len=0
-            //Give one charcter to * and remain at *
-            //at next step it will again be decided from both these cases
+            
         }
-        return dp[i][j]=0;
+        return dp[i][j]=false;
         
     }
     
