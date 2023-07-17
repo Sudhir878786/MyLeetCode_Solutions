a766cce629470a14a0b881b1cee25976365c01e7
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-07-17 13:47:18 +0530

    Time: 95 ms (52.82%), Space: 10.3 MB (44.16%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
-
-int dfs(vector<int>& cuts, int i, int j,vector<vector<int>>&dp) {
-    if (j - i <= 1)
-        return 0;
-    if (!dp[i][j]) {
-        dp[i][j] = dp[i][j];
-    }
-        for (auto k = i + 1; k < j; ++k)
-            dp[i][j] = min(dp[i][j], 
-                cuts[j] - cuts[i] + dfs(cuts, i, k,dp) + dfs(cuts, k, j,dp));
-    
-    return dp[i][j];
-}
-int minCost(int n, vector<int>& cuts) {
-    int c=cuts.size();
-       cuts.push_back(n);
-    cuts.insert(cuts.begin(),0);
-    sort(cuts.begin(),cuts.end());
-    
-    vector<vector<int>> dp(c+2,vector<int>(c+2,0));
-    
-    for(int i=c; i>=1; i--){
-        for(int j=1; j<=c; j++){
-            
-            if(i>j) continue;
-            
-            int mini = INT_MAX;
-    
-            for(int ind=i; ind<=j; ind++){
-        
-                int ans = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
-        
-                mini = min(mini, ans);
-        
-            }
-    
-            dp[i][j] = mini;
+    int func(int i,int j,vector<int>&cuts,vector<vector<int>>&dp)
+    {
+        if(i>j){
+            return 0;
         }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=INT_MAX;
+        for(int k=i;k<=j;k++){
+            int cur=cuts[j+1]-cuts[i-1]+func(i,k-1,cuts,dp)+func(k+1,j,cuts,dp);
+            ans=min(ans,cur);
+        }
+        return dp[i][j]= ans;
     }
-    
-    return dp[1][c];
-
-
-}
-    
+    int minCost(int n, vector<int>& cuts) {
+        int c=cuts.size();
+        cuts.insert(cuts.begin(),0);
+        cuts.push_back(n);
+        sort(cuts.begin(),cuts.end());
+        vector<vector<int>>dp(c+2,vector<int>(c+2,-1));
+        return func(1,c,cuts,dp);
+    }
 };
