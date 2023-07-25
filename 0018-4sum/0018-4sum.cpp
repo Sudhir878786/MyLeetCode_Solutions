9153f0668ac0b54d5d8592f3bca1e534ab322f22
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-07-25 11:55:49 +0530

    Time: 199 ms (32.52%), Space: 13.1 MB (93.00%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,42 +1,54 @@
-class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-3;i++){
-            if(i>0 and nums[i]==nums[i-1]){
-                continue;
-            }
-            for(int j=i+1;j<n-2;j++){
-                if(j>i+1 and nums[j]==nums[j-1]){
+class Solution
+{
+    public:
+        vector<vector < int>> fourSum(vector<int> &nums, int target)
+        {
+            vector<vector < int>> ans;
+            int n = nums.size();
+            sort(nums.begin(), nums.end());
+            for (int i = 0; i < n - 3; i++)
+            {
+                if (i > 0 and nums[i] == nums[i - 1])
+                {
                     continue;
                 }
-                int k=j+1;
-                int s=n-1;
-                while(k<s){
-                    if((long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[s]==target){
-                        ans.push_back({nums[i],nums[j],nums[k],nums[s]});
-                        while(k<s and nums[k]==nums[k+1]){
-                            // continue;
+                for (int j = i + 1; j < n - 2; j++)
+                {
+                    if (j > i + 1 and nums[j] == nums[j - 1])
+                    {
+                        continue;
+                    }
+                    int k = j + 1;
+                    int s = n - 1;
+                    while (k < s)
+                    {
+                        if ((long long) nums[i] + (long long) nums[j] + (long long) nums[k] + (long long) nums[s] == target)
+                        {
+                            ans.push_back({ nums[i],
+                                nums[j],
+                                nums[k],
+                                nums[s] });
+                            while (k < s and nums[k] == nums[k + 1])
+                            {
+                               	// continue;
+                                k++;
+                            }
+                            while (k < s and nums[s] == nums[s - 1])
+                            {
+                               	// continue;
+                                s--;
+                            }
                             k++;
-                        }
-                        while(k<s and nums[s]==nums[s-1]){
-                            // continue;
                             s--;
                         }
-                        k++;
-                        s--;
-                        
+                        else if ((long long) nums[i] + (long long) nums[j] + (long long) nums[k] + (long long) nums[s] < target)
+                        {
+                            k++;
+                        }
+                        else s--;
                     }
-                    else if((long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[s]<target){
-                        k++;
-                    }
-                    
-                    else s--;
                 }
             }
+            return ans;
         }
-        return ans;
-    }
 };
