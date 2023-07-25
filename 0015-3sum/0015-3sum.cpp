637b6aad0b26d0b171f4c0ef7af1c836db43b161
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-07-25 11:23:09 +0530

    Time: 301 ms (40.78%), Space: 24 MB (86.22%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,26 +4,32 @@ public:
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
-            if(i>0 and nums[i]==nums[i-1])continue;
+            if(i>0 and nums[i]==nums[i-1]){
+                continue;
+            }
             int j=i+1;
             int n=nums.size()-1;
             while(j<n){
-                if(nums[i]+nums[j]+nums[n]==0){
+                if((nums[i]+nums[j]+nums[n])==0){
                     ans.push_back({nums[i],nums[j],nums[n]});
-                    while(j<n and (nums[j]==nums[j+1])){
+                    while(j<n and nums[j]==nums[j+1]){
                         j++;
                     }
-                    while(j<n and (nums[n]==nums[n-1])){
+                    while(j<n and nums[n]==nums[n-1]){
                         n--;
                     }
                     j++;
                     n--;
-                    
                 }
                 else if((nums[i]+nums[j]+nums[n])<0){
                     j++;
-                }else n--;
+                }
+                else {
+                    n--;
+                }
+                
             }
+            
         }
         return ans;
     }
