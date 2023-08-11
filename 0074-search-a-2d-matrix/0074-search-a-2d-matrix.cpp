84bcfe4145e40d94fd2d8a131d24b54c45ee960a
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-11 13:01:13 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (7.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,14 +4,20 @@ public:
         int n=mat.size();
         int m=mat[0].size();
     
-   for(int i=0; i<n; i++){
-       for(int j=0; j<m; j++){
-           
-           if(mat[i][j]==target)
-               return true;
-       }
-   }
-   return false;
+        int i=0,j=m-1;
+        while(j>=0 and i<n){
+            if(mat[i][j]==target){
+                return true;
+
+            }
+            else if(mat[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
 
     }
 };
