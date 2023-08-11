05fe1471902ee9e87e7b9736cf2785187540e13d
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-11 10:36:43 +0530

    Time: 12 ms (81.17%), Space: 18.7 MB (24.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int func(int i,vector<int>&coins,int amount,vector<vector<int>>&dp)
+    int func(int i,vector<int>&nums,int k,vector<vector<int>>&dp)
     {
         if(i==0){
-            return  (amount%coins[0]==0);
+            return (k%nums[0]==0);
         }
-        if(dp[i][amount]!=-1){
-            return dp[i][amount];
+        if(dp[i][k]!=-1){
+            return dp[i][k];
         }
-        int nott=0+func(i-1,coins,amount,dp);
+        int nott=0+func(i-1,nums,k,dp);
         int take=0;
-        if(coins[i]<=amount){
-            take=func(i,coins,amount-coins[i],dp);
+        if(nums[i]<=k)
+        {
+            take=func(i,nums,k-nums[i],dp);
         }
-        return dp[i][amount] = take+nott;
+        return dp[i][k]= take+nott;
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
         return func(n-1,coins,amount,dp);
-        
     }
 };
