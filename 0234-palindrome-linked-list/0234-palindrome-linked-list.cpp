bbdfa05f8742f237645df1308adee4eb56162145
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-14 11:09:13 +0530

    Time: 193 ms (82.33%), Space: 121.3 MB (34.38%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,21 +10,33 @@
  */
 class Solution {
 public:
+    ListNode* cur;
+    bool func(ListNode*head)
+    {
+        if(head==NULL){
+            return true;
+        }
+        bool f=func(head->next ) and (cur->val==head->val);
+        cur=cur->next;
+        return f;
+    }
     bool isPalindrome(ListNode* head) {
-        stack<ListNode*>st;
-        ListNode*cur=head;
-        while(cur!=NULL){
-            st.push(cur);
-            cur=cur->next;
-        }
-        while(head!=NULL and !st.empty()){
-            auto it=st.top()->val;
-            st.pop();
-            if(head->val!=it){
-                return false;
-            }
-            head=head->next;
-        }
-        return true;
+        // stack<ListNode*>st;
+        // ListNode*cur=head;
+        // while(cur!=NULL){
+        //     st.push(cur);
+        //     cur=cur->next;
+        // }
+        // while(head!=NULL and !st.empty()){
+        //     auto it=st.top()->val;
+        //     st.pop();
+        //     if(head->val!=it){
+        //         return false;
+        //     }
+        //     head=head->next;
+        // }
+        // return true;
+        cur=head;
+        return func(head);
     }
 };
