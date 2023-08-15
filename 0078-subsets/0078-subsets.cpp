16ead7298c090332848fa9a301cd9b445534b3df
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-15 21:15:07 +0530

    Time: 4 ms (45.56%), Space: 15.9 MB (29.46%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+      vector<vector<int>> ans;
+    
+    void sub(vector<int> &nums, int i, vector<int> temp)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        sub(nums, i+1, temp);
+        temp.push_back(nums[i]);
+        sub(nums, i+1, temp);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;       
+        sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
+        return ans;
+    }
+};
