ac36d65d515d6cb9f2c78ac3819f224860ccaefa
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-17 10:28:49 +0530

    Time: 65 ms (87.06%), Space: 31.6 MB (71.08%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,45 +4,34 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        
         queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     dist[i][j]=0;
                     q.push({{i,j},0});
-                    // vis[i][j]=1;
                 }
-                // else vis[i][j]=0;
             }
         }
-        int delrow[] = {-1, 0, +1, 0}; 
-	    int delcol[] = {0, +1, 0, -1}; 
-	    
-	    // traverse till queue becomes empty
-	    while(!q.empty()) {
-	        int row = q.front().first.first; 
-	        int col = q.front().first.second; 
-	        int steps = q.front().second; 
-	        q.pop(); 
-	        // dist[row][col] = steps; 
-	        // for all 4 neighbours
-	        for(int i = 0;i<4;i++) {
-	            int nrow = row + delrow[i]; 
-	            int ncol = col + delcol[i]; 
-	            // check for valid unvisited cell
-	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-	           ) {
-	                // vis[nrow][ncol] = 1; 
-                    if(dist[nrow][ncol]>dist[row][col]+1){
-                        dist[nrow][ncol]=dist[row][col]+1;
-                        q.push({{nrow, ncol}, steps+1});  
+        int dirx[]={-1,0,1,0};
+        int diry[]={0,1,0,-1};
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                int x=q.front().first.first;
+                int y=q.front().first.second;
+                int cost=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int r=x+dirx[i];
+                    int c=y+diry[i];
+                    if(r>=0 and c>=0 and r<n and c<m and dist[r][c]>dist[x][y]+1){
+                        dist[r][c]=dist[x][y]+1;
+                        q.push({{r,c},c+1});
                     }
-	                
-	            }
-	        }
-	    }
-	    // return distance matrix
-	    return dist; 
+                }
+            }
+        }
+        return dist;
     }
 };
