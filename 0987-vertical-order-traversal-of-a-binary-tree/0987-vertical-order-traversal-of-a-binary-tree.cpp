ea1388baf5baec2b6c8840959f3fcee01bbb8af1
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-18 18:16:38 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (70.83%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,38 +1,37 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-
-        void helper(TreeNode *root, int i, int l, map<int, vector<pair<int, int>>> &mp)
-        {
-            if (!root) return;
-            mp[i].push_back({ l,
-                root->val });
-            helper(root->left, i - 1, l + 1, mp);
-            helper(root->right, i + 1, l + 1, mp);
-        }
-    vector<vector < int>> verticalTraversal(TreeNode *root)
+class Solution {
+public:
+    void func(TreeNode*root,int ind,int lev,map<int,vector<pair<int,int>>>&mp)
     {
-        vector<vector < int>> res;
-        map<int, vector<pair<int, int>>> mp;
-        helper(root, 0, 0, mp);
+        if(!root){
+            return;
+        }
+        mp[ind].push_back({lev,root->val});
+        func(root->left,ind-1,lev+1,mp);
+        func(root->right,ind+1,lev+1,mp);
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>>ans;
+        map<int,vector<pair<int,int>>>mp;
+        func(root,0,0,mp);
+       
         for (auto x: mp)
         {
             vector<int> v;
             sort(begin(x.second), end(x.second));
             for (auto i: x.second) v.push_back(i.second);
-            res.push_back(v);
+            ans.push_back(v);
         }
-        return res;
+        return ans;
     }
 };
