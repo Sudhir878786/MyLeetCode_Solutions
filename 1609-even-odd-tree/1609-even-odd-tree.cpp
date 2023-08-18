17cfce00a760baeb0d98e23de956f51185a6be9e
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-19 02:38:47 +0530

    Time: 234 ms (75.82%), Space: 169.6 MB (13.05%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*>q;
+        vector<vector<int>>ans;
+        if(root==NULL) return true;
+        q.push(root);
+        int j=0;
+        while(q.empty()!=true)
+        {   vector<int>v;
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {   TreeNode* t=q.front();q.pop();
+                v.push_back(t->val);
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+         if(j%2==0)
+         {
+             for(int k=0;k<v.size()-1;k++)
+             {
+                 if(v[k]%2==0)
+                     return false;
+                 if(v[k]>=v[k+1])
+                     return false;
+             }
+             if(v[v.size()-1]%2==0)
+                 return false;
+         }
+         else
+         {
+              for(int k=0;k<v.size()-1;k++)
+             {
+                 if(v[k]%2==1)
+                     return false;
+                 if(v[k]<=v[k+1])
+                     return false;
+             }
+             if(v[v.size()-1]%2==1)
+                 return false;
+         }
+         j++;
+            ans.push_back(v);
+        }
+        return true;
+    }
+};
