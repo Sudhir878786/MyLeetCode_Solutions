3624e4c87995e485830b3ddc1ce8d9b2eba8ec69
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-20 18:37:38 +0530

    Time: 141 ms (92.58%), Space: 114.4 MB (76.55%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -1,43 +1,57 @@
-class NodeValue {
-public: 
-    int minNode, maxNode, maxSum;
-    
-    NodeValue(int minNode, int maxNode, int maxSum)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Node{
+  public:
+    int minn, maxx,size;
+    Node(int minn,int maxx,int size)
     {
-        this->minNode = minNode;
-        this->maxNode = maxNode;
-        this->maxSum = maxSum;
+        this->minn=minn;
+        this->maxx=maxx;
+        this->size=size;
     }
 };
+// intutioon 
 
+
+// we traverse from the bottom to up and ccheck for the maximum BST size
+// we find the min max node in the left and min node in the right
 class Solution {
-    
-private:
-    NodeValue maxSumBSTHelper(TreeNode* root)
+public:
+    int ans=0;
+    Node func(TreeNode*root)
     {
-        if(!root) return NodeValue(INT_MAX, INT_MIN, 0);
-        
-        auto left = maxSumBSTHelper(root->left);
-        auto right = maxSumBSTHelper(root->right);
-        
-        if(left.maxNode < root->val && root->val < right.minNode)
-        {
-            //if BT is BST
-            sum = max(sum, root->val + left.maxSum + right.maxSum);
-            
-            return NodeValue(min(root->val, left.minNode), max(root->val, right.maxNode), root->val + left.maxSum + right.maxSum);
-            
+        if(!root){
+            return Node(INT_MAX,INT_MIN,0);
         }
         
-        return NodeValue(INT_MIN, INT_MAX, max(left.maxSum, right.maxSum));   
-    }
-    
-public:
-    int sum=0;
-    int maxSumBST(TreeNode* root) 
-    {
-        maxSumBSTHelper(root);
-        return sum>0 ? sum : 0;
+        auto left=func(root->left);  // bittom to top
+        auto right=func(root->right);
+        // if the cur tree is BST
+        if(root->val>left.maxx and root->val<right.minn)
+        {
+            ans=max(ans,left.size+right.size+root->val);
+            return Node(min(root->val,left.minn),max(root->val,right.maxx),left.size+right.size+root->val);
+        }
+        return Node(INT_MIN,INT_MAX,max(left.size,right.size));
         
+    }
+    int maxSumBST(TreeNode* root) {
+        func(root);
+        if(ans>0){
+            return ans;
+        }
+        return 0;
+      
     }
 };
