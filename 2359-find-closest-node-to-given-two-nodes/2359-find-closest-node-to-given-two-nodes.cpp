b4dd2df36f3932d7568123c0bb2926a9e195ee3b
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-23 11:44:25 +0530

    Time: 130 ms (85.61%), Space: 97.5 MB (81.85%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,44 +1,33 @@
-class Solution
-{
-    public:
-
-        int closestMeetingNode(vector<int> &edges, int node1, int node2)
-        {
-            int n=edges.size();
-            vector<int> dist1(n, -1);
-            vector<int> dist2(n, -1);
-            DFS(edges, node1, dist1, 0);
-            DFS(edges, node2, dist2, 0);
-            int minDist = INT_MAX;
-            int minNode = -1;
-            for (int i = 0; i < n; i++)
-            {
-                if (dist1[i] != -1 && dist2[i] != -1)
-                {
-                    if (max(dist1[i], dist2[i]) < minDist)
-                    {
-                        minDist = max(dist1[i], dist2[i]);
-                        minNode = i;
-                    }
-                    // else if (max(dist1[i], dist2[i]) == minDist && i < minNode)
-                    // {
-                    //     minNode = i;
-                    // }
-                }
-            }
-            return minNode;
-        }
-
-    void DFS(vector<int> &edges, int node, vector<int> &dist, int d)
+class Solution {
+public:
+    void dfs(vector<int>&edges,int node,vector<int>&d1,int d)
     {
-        if (dist[node] != -1)
-        {
+        if(d1[node]!=-1){
             return;
         }
-        dist[node] = d;
-        if (edges[node] != -1)
+        d1[node]=d;
+        if(edges[node]!=-1)
         {
-            DFS(edges, edges[node], dist, d + 1);
+            dfs(edges,edges[node],d1,d+1);
         }
     }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n=edges.size();
+        vector<int>d1(n,-1);
+        vector<int>d2(n,-1);
+        dfs(edges,node1,d1,0);
+        dfs(edges,node2,d2,0);
+        int node=-1;
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(d1[i]!=-1 and d2[i]!=-1){
+                if(max(d1[i],d2[i])<ans){
+                    ans=max(d1[i],d2[i]);
+                    node=i;
+                }
+            }
+        }
+        return node;
+    }
+    
 };
