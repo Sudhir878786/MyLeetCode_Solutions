e0b45d74989291d3fb6cdddb3e709e61a6b82877
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-30 01:03:11 +0530

    Time: 3 ms (90.52%), Space: 8.9 MB (32.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,26 +4,46 @@ class Solution
         vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
         {
             vector<int> ans;
-            for (int i = 0; i < nums1.size(); i++)
-            {
-                for (int j = 0; j < nums2.size(); j++)
-                {
-                    if (nums1[i] == nums2[j])
-                    {
-                        int cur = -1;
-                        for (int k = j; k < nums2.size(); k++)
-                        {
+//             for (int i = 0; i < nums1.size(); i++)
+//             {
+//                 for (int j = 0; j < nums2.size(); j++)
+//                 {
+//                     if (nums1[i] == nums2[j])
+//                     {
+//                         int cur = -1;
+//                         for (int k = j; k < nums2.size(); k++)
+//                         {
 
-                            if (nums2[k] > nums2[j])
-                            {
-                                cur = nums2[k];
-                                // cout<<"sjfd"<<endl;
-                                break;
-                            }
-                        }
-                        ans.push_back(cur);
-                    }
+//                             if (nums2[k] > nums2[j])
+//                             {
+//                                 cur = nums2[k];
+//                                 // cout<<"sjfd"<<endl;
+//                                 break;
+//                             }
+//                         }
+//                         ans.push_back(cur);
+//                     }
+//                 }
+//             }
+//             return ans;
+            
+            stack<int>st;
+            unordered_map<int,int>mp;
+            for(int i=nums2.size()-1;i>=0;i--){
+                while(!st.empty() and st.top()<=nums2[i]){
+                    st.pop();
                 }
+                if(st.empty()){
+                    mp[nums2[i]]=-1;
+                }
+                else{
+                    mp[nums2[i]]=st.top();
+                }
+                    
+                st.push(nums2[i]);
+            }
+            for(int i=0;i<nums1.size();i++){
+                ans.push_back(mp[nums1[i]]);
             }
             return ans;
         }
