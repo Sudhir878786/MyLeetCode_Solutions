3d7aa740e2535ab2140c80d863409d4237adebc5
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-29 14:50:26 +0530

    Time: 5 ms (15.86%), Space: 5.9 MB (45.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,25 @@
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        if(n==0){
-            return 0;
-        }
-        while(n!=1){
-            if(n%2!=0){
+class Solution
+{
+    public:
+        bool isPowerOfTwo(int n)
+        {
+           	// if(n==0){
+           	//     return 0;
+           	// }
+           	// while(n!=1){
+           	//     if(n%2!=0){
+           	//         return false;
+           	//     }
+           	//     n/=2;
+           	// }
+           	// return true;
+            if(n==0){
                 return false;
             }
-            n/=2;
+            if ((long long)(n & ((long long)n - 1)) == 0)
+            {
+                return true;
+            }
+            return false;
         }
-        return true;
-    }
 };
