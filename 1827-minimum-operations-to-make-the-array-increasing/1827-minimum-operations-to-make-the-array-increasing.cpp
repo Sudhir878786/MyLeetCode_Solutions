6936a94fdc1088625059ae590b98a60568875adc
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-08-30 14:54:03 +0530

    Time: 12 ms (72.62%), Space: 15.8 MB (15.10%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -3,9 +3,9 @@ public:
     int minOperations(vector<int>& nums) {
         int n=nums.size();
         int ans=0;
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<n;i++){
             if(nums[i]<=nums[i-1]){
-                ans+=(nums[i-1]-nums[i]+1);
+                ans+=((nums[i-1]-nums[i])+1);
                 nums[i]=nums[i-1]+1;
             }
         }
