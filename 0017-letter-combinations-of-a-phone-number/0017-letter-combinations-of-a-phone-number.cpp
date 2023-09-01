f5668e346816d2373e449412b6623ffd264256d9
Sudhir Sharma <92601949+Sudhir878786@users.noreply.github.com>
2023-09-01 12:47:53 +0530

    Time: 3 ms (39.12%), Space: 6.6 MB (45.68%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<string>ans;
-    void func(string digits,string &cur,int ind,vector<string>&let)
+    void func(string digits,int ind,vector<string>&let,string temp)
     {
         if(ind==digits.size()){
-            ans.push_back(cur);
+            ans.push_back(temp);
             return;
         }
-        string temp=let[digits[ind]-'0'];
-        for(int i=0;i<temp.size();i++){
-            cur.push_back(temp[i]);
-            func(digits,cur,ind+1,let);
-            cur.pop_back();
+        string cur=let[digits[ind]-'0'];
+        // cout<<cur<<endl;
+        for(int i=0;i<cur.size();i++){
+            temp.push_back(cur[i]);
+            func(digits,ind+1,let,temp);
+            temp.pop_back();
+            
         }
-        
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> let={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        if(digits.empty())
+         vector<string> let={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if(digits==""){
             return {};
-        string cur="";
-        func(digits,cur,0,let);
-        return ans;
-       
+        }
+         func(digits,0,let,"");
+        return  ans;
+
     }
 };
